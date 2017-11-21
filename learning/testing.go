c1bb0c126505fcce5bb5ce56a315d6182392959f
Marcus Willock <marcuswillock@qq.com>
2017-11-22 02:25:39 +0800

    I was able to highlight a box

--- learning/testing.go
+++ learning/testing.go
@@ -5,6 +5,15 @@ import (
 	"log"
 )
 
+func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
+	_, err := g.SetCurrentView(name)
+	if err != nil && err != gocui.ErrUnknownView {
+		return nil, err
+	}
+	return g.SetViewOnTop(name)
+
+}
+
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 	_, err := g.SetView("question", -1, -1, maxX, int(0.5*float32(maxY)))
@@ -13,7 +22,7 @@ func layout(g *gocui.Gui) error {
 	}
 
 	//Answer Box A
-	answerA, err := g.SetView("answerA", -1, int(0.5*float32(maxY)), int(0.5*float32(maxX)), int(0.75*float32(maxY)))
+	answerA, err := g.SetView("answerA", -1, int(0.5*float32(maxY)), int(0.5*float32(maxX)), int(0.73*float32(maxY)))
 	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
@@ -21,8 +30,14 @@ func layout(g *gocui.Gui) error {
 	answerA.Editable = false
 	answerA.Wrap = true
 
+	//Put answer box A on top
+	_, err = setCurrentViewOnTop(g, "answerA")
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
 	//Answer Box B
-	answerB, err := g.SetView("answerB", int(0.5*float32(maxX)), int(0.5*float32(maxY)), maxX, int(0.75*float32(maxY)))
+	answerB, err := g.SetView("answerB", int(0.5*float32(maxX)), int(0.5*float32(maxY)), maxX, int(0.73*float32(maxY)))
 	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
@@ -31,7 +46,7 @@ func layout(g *gocui.Gui) error {
 	answerB.Wrap = true
 
 	//Answer Box C
-	answerC, err := g.SetView("answerC", -1, int(0.75*float32(maxY)), int(0.5*float32(maxX)), maxY)
+	answerC, err := g.SetView("answerC", -1, int(0.77*float32(maxY)), int(0.5*float32(maxX)), maxY)
 	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
@@ -40,7 +55,7 @@ func layout(g *gocui.Gui) error {
 	answerC.Wrap = true
 
 	//Answer Box D
-	answerD, err := g.SetView("answerD", int(0.5*float32(maxX)), int(0.75*float32(maxY)), maxX, maxY)
+	answerD, err := g.SetView("answerD", int(0.5*float32(maxX)), int(0.77*float32(maxY)), maxX, maxY)
 	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
@@ -62,6 +77,11 @@ func main() {
 	}
 	defer g.Close()
 
+	//Turn highlighting on and set color
+	g.Highlight = true
+	g.SelFgColor = gocui.ColorGreen
+
+	//PAss in the layout I want ot use
 	g.SetManagerFunc(layout)
 
 	err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
