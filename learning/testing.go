a40baec833255d6b5cdffda758d91543f18f924c
Marcus Willock <marcuswillock@qq.com>
2017-11-22 14:12:57 +0800

    I can now switch between answers

--- learning/testing.go
+++ learning/testing.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jroimartin/gocui"
 	"log"
 )
 
+const (
+	boxA = "answerA"
+	boxB = "answerB"
+	boxC = "answerC"
+	boxD = "answerD"
+)
+
+var (
+	boxesView  = []string{boxA, boxB, boxC, boxD}
+	activeView = 0
+)
+
+func nextView(g *gocui.Gui, v *gocui.View) error {
+	nextIndex := (activeView + 1) % len(boxesView)
+	name := boxesView[nextIndex]
+
+	_, err := setCurrentViewOnTop(g, name)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	//Debug
+	out, err := g.View(boxB)
+	if err != nil {
+		return nil
+	}
+	fmt.Fprintln(out, "Going from view "+v.Name()+" to "+name)
+
+	activeView = nextIndex
+	return nil
+}
+
 func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
 	_, err := g.SetCurrentView(name)
-	if err != nil && err != gocui.ErrUnknownView {
+	if err != nil {
 		return nil, err
 	}
 	return g.SetViewOnTop(name)
@@ -22,46 +55,51 @@ func layout(g *gocui.Gui) error {
 	}
 
 	//Answer Box A
-	answerA, err := g.SetView("answerA", -1, int(0.5*float32(maxY)), int(0.5*float32(maxX)), int(0.73*float32(maxY)))
-	if err != nil && err != gocui.ErrUnknownView {
-		return err
-	}
-	answerA.Title = "A"
-	answerA.Editable = false
-	answerA.Wrap = true
+	if v, err := g.SetView(boxA, -1, int(0.5*float32(maxY)), int(0.5*float32(maxX)), int(0.73*float32(maxY))); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
 
-	//Put answer box A on top
-	_, err = setCurrentViewOnTop(g, "answerA")
-	if err != nil && err != gocui.ErrUnknownView {
-		return err
+		v.Title = "A"
+		v.Editable = false
+		v.Wrap = true
+
+		if _, err := setCurrentViewOnTop(g, boxA); err != nil {
+			return err
+		}
 	}
 
 	//Answer Box B
-	answerB, err := g.SetView("answerB", int(0.5*float32(maxX)), int(0.5*float32(maxY)), maxX, int(0.73*float32(maxY)))
-	if err != nil && err != gocui.ErrUnknownView {
-		return err
+	if v, err := g.SetView(boxB, int(0.5*float32(maxX)), int(0.5*float32(maxY)), maxX, int(0.73*float32(maxY))); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Title = "B"
+		v.Editable = true
+		v.Wrap = true
+
 	}
-	answerB.Title = "B"
-	answerB.Editable = false
-	answerB.Wrap = true
 
 	//Answer Box C
-	answerC, err := g.SetView("answerC", -1, int(0.77*float32(maxY)), int(0.5*float32(maxX)), maxY)
-	if err != nil && err != gocui.ErrUnknownView {
-		return err
+	if v, err := g.SetView(boxC, -1, int(0.77*float32(maxY)), int(0.5*float32(maxX)), maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "C"
+		v.Editable = false
+		v.Wrap = true
 	}
-	answerC.Title = "C"
-	answerC.Editable = false
-	answerC.Wrap = true
 
 	//Answer Box D
-	answerD, err := g.SetView("answerD", int(0.5*float32(maxX)), int(0.77*float32(maxY)), maxX, maxY)
-	if err != nil && err != gocui.ErrUnknownView {
-		return err
+	if v, err := g.SetView(boxD, int(0.5*float32(maxX)), int(0.77*float32(maxY)), maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "D"
+		v.Editable = false
+		v.Wrap = true
 	}
-	answerD.Title = "D"
-	answerD.Editable = false
-	answerD.Wrap = false
 
 	return nil
 }
@@ -84,9 +122,16 @@ func main() {
 	//PAss in the layout I want ot use
 	g.SetManagerFunc(layout)
 
+	//Quit Keybinding
 	err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	//Toggle Answers Keybinding
+	err = g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, nextView)
+	if err != nil {
+		log.Panicln(err)
 	}
 
 	err = g.MainLoop()
