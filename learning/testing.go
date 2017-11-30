3ed7280868ee46c40a055717a8b415aad3fff375
Marcus Willock <crazcalm@gmail.com>
2017-12-01 01:36:30 +0800

    I am really tired and I made a ton of stupid mistakes, but I have also made a lot of progress.
    I will clean the code up later. For now, good night

--- learning/testing.go
+++ learning/testing.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"github.com/jroimartin/gocui"
 	"log"
+	"strings"
+	"os"
+	"encoding/csv"
+	"io/ioutil"
+	"io"
 )
 
 const (
@@ -15,13 +20,108 @@ const (
 	questionBox   = "question"
 )
 
+//Answer -- struct to hold an answer
+type Answer struct {
+	Answer string
+	Correct bool
+}
+
+//Question -- struct to hold a question and its answers
+type Question struct {
+	Question string
+	Answers []Answer
+	Explaination string
+}
+
 var (
 	boxesView  = []string{boxA, boxB, boxC, boxD}
 	activeView = 0
+	questions = []Question{}
 )
+
+func readCSV() [][]string {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(files)
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+	testFile := files[1].Name()
+	_, err = os.Stat(testFile)
+	if os.IsNotExist(err){
+		log.Fatalf("file: %s does not exist", testFile)
+	}
+
+	file, err := os.Open(testFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(file)
+
+	var records [][]string
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+		records = append(records, record)		
+	}
+
+	return records
+}
+
+func createQuestions(records [][]string) {
+	for i:=1; i < len(records); i++ {
+
+		a1 := Answer{records[i][1], true}
+		a2 := Answer{records[i][2], false}
+		a3 := Answer{records[i][3], false}
+		a4 := Answer{records[i][4], false}
+	
+		question := Question{
+			records[i][0],
+			[]Answer{a1, a2, a3, a4},
+			records[i][5],
+		}
+		questions = append(questions, question)
+	}
+}
+
+func isViewInSlice(viewNames []string, v *gocui.View) bool {
+	result := false
+	for _, name := range viewNames {
+		if strings.EqualFold(name, v.Name()) == true {
+			result = true
+		}
+	}
+	return result
+}
 
 func selectAnswer(g *gocui.Gui, v *gocui.View) error {
 	fmt.Fprintln(v, "Selected")
+
+	views := g.Views()
+	for i, view := range views {
+
+		//Find the question answer boxes
+		if isViewInSlice(boxesView, view){
+			view.Clear() //clear the box
+			fmt.Fprintf(view, "%d - %s -- changed!", i, view.Name())
+		}
+
+		if strings.EqualFold(questionBox,view.Name()){
+			view.Clear()  //clear the question box
+			fmt.Fprintf(view, "%d - %s -- question box", i, view.Name())
+		}
+		
+	}
 	return nil
 }
 
@@ -69,7 +169,7 @@ func layout(g *gocui.Gui) error {
 			return err
 		}
 		v.Title = "Question"
-		fmt.Fprintln(v, "Test question")
+		fmt.Fprintln(v, questions[0].Question)
 	}
 
 	//Answer Box A
@@ -135,6 +235,14 @@ func quit(g *gocui.Gui, v *gocui.View) error {
 }
 
 func main() {
+	//Get questions
+	data := readCSV()
+	if len(data) == 0 {
+		log.Fatal("unable to read csv file")
+	}
+	fmt.Println(data)
+	createQuestions(data)	
+
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
@@ -145,7 +253,7 @@ func main() {
 	g.Highlight = true
 	g.SelFgColor = gocui.ColorGreen
 
-	//PAss in the layout I want ot use
+	//Pass in the layout I want ot use
 	g.SetManagerFunc(layout)
 
 	//Quit Keybinding
