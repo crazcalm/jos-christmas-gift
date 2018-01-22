efff425e89c7804f7fd6049d36d1e4dfcc631efc
Marcus Willock <crazcalm@gmail.com>
2018-01-23 02:08:43 +0800

    I am going to start replacing the old version with the new one, but before I do that, I have a number of little things to take care of

---
+++ version2/jo/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/crazcalm/term-quiz"
+	"github.com/crazcalm/term-quiz/questions"
+	"github.com/jroimartin/gocui"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+	//Get gui driver
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	//Csv file paths
+	//abcd.csv  error.csv  fill_in_the_blank.csv  true_false.csv
+	path1 := filepath.Join("questions", "abcd.csv")
+	path2 := filepath.Join("questions", "true_false.csv")
+	path3 := filepath.Join("questions", "fill_in_the_blank.csv")
+
+	//Need to create questions
+	quiz.Questions, err = questions.CreateQuestions(quiz.Questions, path1, path2, path3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Shuffle Questions
+	err = quiz.Questions.Shuffle()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Need to initialize screen
+	err = quiz.Init(g)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Run main loop
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
